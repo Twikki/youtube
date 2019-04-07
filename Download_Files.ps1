f21e94e46ab8c55ab2a601a9ac0ed53c4a1b0615
Daniel Jean Schmidt <46316044+Twikki@users.noreply.github.com>
2019-04-07 20:25:49 +0200

    Add files via upload

---
+++ Download_Files.ps1
@@ -0,0 +1,7 @@
+
+# Creates Zabbix DIR
+mkdir c:\Github_folder
+
+
+# Downloads version 4.0.4 from Zabbix.com
+wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile C:\Github_folder\zabbix-4.0.4.zip
---
+++ Github_Private.ps1
@@ -0,0 +1,7 @@
+﻿# Created by Daniel Jean Schmidt
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$Script = Invoke-RestMethod https://api.github.com/repos/Twikki/youtubetest/contents/Download_Files.ps1?access_token=yourtokenhere -Headers @{”Accept”= “application/vnd.github.v3.raw”}
+
+Invoke-Expression $Script
