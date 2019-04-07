0c6d63b59532aec2b52effe7cc743670d26f677f
Daniel Jean Schmidt <46316044+Twikki@users.noreply.github.com>
2019-04-07 19:48:46 +0200

    Update Download_Files.ps1

--- Download_Files.ps1
+++ Download_Files.ps1
@@ -5,3 +5,5 @@ mkdir c:\Github_folder
 
 # Downloads version 4.0.4 from Zabbix.com
 wget "https://www.zabbix.com/downloads/4.0.4/zabbix_agents-4.0.4-win-amd64.zip" -outfile C:\Github_folder\zabbix-4.0.4.zip
+
+mkdir c:\Github_folder\Testfolder
